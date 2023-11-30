c51cf2f91bcb76b9ff672141dd6381217d929e08
gtpass <131739976+gtpass@users.noreply.github.com>
2023-11-30 22:12:18 +0800

    Update main.go

--- .geoip/main.go
+++ .geoip/main.go
@@ -59,7 +59,7 @@ func get(downloadURL *string) ([]byte, error) {
 
 func download(release *github.RepositoryRelease) ([]byte, error) {
 	geoipAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "Country.mmdb"
+		return *it.Name == "geoip.dat"
 	})
 	if geoipAsset == nil {
 		return nil, E.New("Country.mmdb not found in upstream release ", release.Name)
