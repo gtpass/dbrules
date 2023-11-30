c269753d4a15fe611e82e2fd826d3064d08c38b3
gtpass <131739976+gtpass@users.noreply.github.com>
2023-11-30 18:13:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -252,7 +252,8 @@ func release(source string, destination string, output string, ruleSetOutput str
 }
 
 func main() {
-	err := release("v2fly/domain-list-community", "gtpass/test", "geosite.db", "rule-set")
+	#err := release("v2fly/domain-list-community", "gtpass/test", "geosite.db", "rule-set")
+	err := release("Loyalsoldier/v2ray-rules-dat", "gtpass/test", "geosite.db", "rule-set")
 	if err != nil {
 		log.Fatal(err)
 	}
